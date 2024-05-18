072074394824442ba6c74a3cc883744b5b56beb0
Yassine MAALOUM <maaloum.yassine@gmail.com>
2024-05-18 14:28:34 +0100

    secret

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -8,6 +8,12 @@ service mariadb stop
 
 # service --status-all
 
+DB_NAME=$(cat $MYSQL_DATABASE)
+DB_USER=$(cat $MYSQL_USER)
+DB_PASSWORD=$(cat $MYSQL_PASSWORD)
+DB_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD)
+
+
 if [ ! -d "/run/mysqld" ]; then
 	mkdir /run/mysqld;
 fi
@@ -15,11 +21,11 @@ fi
 
 cat << EOF > /tmp/wp.sql
 	FLUSH PRIVILEGES;
-	CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-	CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PASSWORD}';
-	GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
+	CREATE DATABASE IF NOT EXISTS ${DB_NAME};
+	CREATE USER IF NOT EXISTS '${DB_USER}'@'localhost' IDENTIFIED BY '${USER_PASSWORD}';
+	GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 	FLUSH PRIVILEGES;
-	ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
+	ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOT_PASSWORD}';
 EOF
 
 
@@ -30,7 +36,7 @@ rm -f /tmp/wp.sql;
 
 cat << EOF >> /root/.bashrc
 	alias root='mariadb --user=root -p'
-	alias user='mariadb --user=$MYSQL_USER -p'
+	alias user='mariadb --user=$DB_USER -p'
 EOF
 
 source /root/.bashrc
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -2,18 +2,31 @@
 
 cd /var/www/html/wordpress
 
+DB_NAME=$(cat $MYSQL_DATABASE)
+DB_USER=$(cat $MYSQL_USER)
+DB_PASSWORD=$(cat $MYSQL_PASSWORD)
+DB_HOST=$(cat $LOCALHOST)
+ADMIN_WP=$(cat $ADMIN_WP)
+ADMIN_PASSWORD_WP=$(cat $ADMIN_PASSWORD_WP)
+ADMIN_EMAIL_WP=$(cat $ADMIN_EMAIL_WP)
+USER_EMAIL_WP=$(cat $USER_EMAIL_WP)
+USER_PASSWORD_WP=$(cat $USER_PASSWORD_WP)
+
 if ! wp --allow-root core is-installed; then
 	wp		core download	--allow-root;
 	rm 		-rf wp-config-sample.php
-	sed 	-i "s/define( 'DB_NAME', XXXX );/define( 'DB_NAME', '${MYSQL_DATABASE}' );/" wp-config.php
-	sed 	-i "s/define( 'DB_USER', XXXX );/define( 'DB_USER', '${MYSQL_USER}' );/" wp-config.php
-	sed 	-i "s/define( 'DB_PASSWORD', XXXX );/define( 'DB_PASSWORD', '${MYSQL_PASSWORD}' );/" wp-config.php
-	sed 	-i "s/define( 'DB_HOST', XXXX );/define( 'DB_HOST', '${LOCALHOST}' );/" wp-config.php
+	sed 	-i "s/define( 'DB_NAME', XXXX );/define( 'DB_NAME', '${DB_NAME}' );/" wp-config.php
+	sed 	-i "s/define( 'DB_USER', XXXX );/define( 'DB_USER', '${DB_USER}' );/" wp-config.php
+	sed 	-i "s/define( 'DB_PASSWORD', XXXX );/define( 'DB_PASSWORD', '${DB_PASSWORD}' );/" wp-config.php
+	sed 	-i "s/define( 'DB_HOST', XXXX );/define( 'DB_HOST', '${DB_HOST}' );/" wp-config.php
 fi
 
 wp core install --allow-root --url=${URL_WP} --title=${TITLE}  --admin_user=${ADMIN_WP} --admin_password=${ADMIN_PASSWORD_WP} --admin_email=${ADMIN_EMAIL_WP}
 wp user create --allow-root ${USER_WP}  ${USER_EMAIL_WP} --role=editor --user_pass=${USER_PASSWORD_WP}
 
 rm -rf /tmp/script.sh
+
+# chmod 600 /run/secrets/*
+
 
 php-fpm8.2 -F
