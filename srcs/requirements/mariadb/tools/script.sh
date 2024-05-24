087176d3cfe2ccd9ef778a4f2a751032b0f701e6
Yassine MAALOUM <maaloum.yassine@gmail.com>
2024-05-24 07:50:29 +0100

    update for restart

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,33 +1,18 @@
 #!/bin/sh
 
 
-if [ -f "/etc/init.d/mysql" ] || [ -f "/etc/init.d/mariadb" ]
-then
-    echo "MariaDB est déjà installé sur ce système."
-    exit 0  # Quitter le script avec un code de sortie 0 (aucune erreur)
-fi
-
-
-
-apt-get -y update && apt-get -y install mariadb-server
-sed -i "s|bind-address            = 127.0.0.1|bind-address            = 0.0.0.0|g" /etc/mysql/mariadb.conf.d/50-server.cnf
-
 if [ ! -d "/run/mysqld" ]; then
         mkdir /run/mysqld;
 fi
 
+DB_NAME=$(cat $MYSQL_DATABASE)
+DB_USER=$(cat $MYSQL_USER)
+DB_PASSWORD=$(cat $MYSQL_PASSWORD)
+DB_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD)
 
 service mariadb stop
 
 # service --status-all
-
-# DB_NAME=$(cat $MYSQL_DATABASE)
-# DB_USER=$(cat $MYSQL_USER)
-# DB_PASSWORD=$(cat $MYSQL_PASSWORD)
-# DB_ROOT_PASSWORD=$(cat $MYSQL_ROOT_PASSWORD)
-
-
-
 
 cat << EOF > /tmp/wp.sql
         FLUSH PRIVILEGES;
@@ -38,10 +23,8 @@ cat << EOF > /tmp/wp.sql
         ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASSWORD}';
 EOF
 
-
 mariadbd --user=root --bootstrap < /tmp/wp.sql;
 rm -f /tmp/wp.sql;
-
 
 
 
@@ -52,4 +35,4 @@ EOF
 
 source /root/.bashrc
 
-mariadbd --user=root
+exec "$@"
--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -1,18 +1,21 @@
 #!/bin/sh
 
+
 cd /var/www/html/wordpress
 
-DB_NAME=$(cat $MYSQL_DATABASE)
-DB_USER=$(cat $MYSQL_USER)
-DB_PASSWORD=$(cat $MYSQL_PASSWORD)
-DB_HOST=$(cat $LOCALHOST)
-ADMIN_WP=$(cat $ADMIN_WP)
-ADMIN_PASSWORD_WP=$(cat $ADMIN_PASSWORD_WP)
-ADMIN_EMAIL_WP=$(cat $ADMIN_EMAIL_WP)
-USER_EMAIL_WP=$(cat $USER_EMAIL_WP)
-USER_PASSWORD_WP=$(cat $USER_PASSWORD_WP)
+
+	DB_NAME=$(cat $MYSQL_DATABASE)
+	DB_USER=$(cat $MYSQL_USER)
+	DB_PASSWORD=$(cat $MYSQL_PASSWORD)
+	DB_HOST=$(cat $LOCALHOST)
+	ADMIN_WP=$(cat $ADMIN_WP)
+	ADMIN_PASSWORD_WP=$(cat $ADMIN_PASSWORD_WP)
+	ADMIN_EMAIL_WP=$(cat $ADMIN_EMAIL_WP)
+	USER_EMAIL_WP=$(cat $USER_EMAIL_WP)
+	USER_PASSWORD_WP=$(cat $USER_PASSWORD_WP)
 
 if ! wp --allow-root core is-installed; then
+
 	wp		core download	--allow-root;
 	rm 		-rf wp-config-sample.php
 	sed 	-i "s/define( 'DB_NAME', XXXX );/define( 'DB_NAME', '${DB_NAME}' );/" wp-config.php
@@ -20,12 +23,8 @@ if ! wp --allow-root core is-installed; then
 	sed 	-i "s/define( 'DB_PASSWORD', XXXX );/define( 'DB_PASSWORD', '${DB_PASSWORD}' );/" wp-config.php
 	sed 	-i "s/define( 'DB_HOST', XXXX );/define( 'DB_HOST', '${DB_HOST}' );/" wp-config.php
 fi
+	wp core install --allow-root --url=${URL_WP} --title=${TITLE}  --admin_user=${ADMIN_WP} --admin_password=${ADMIN_PASSWORD_WP} --admin_email=${ADMIN_EMAIL_WP}
+	wp user create --allow-root ${USER_WP}  ${USER_EMAIL_WP} --role=editor --user_pass=${USER_PASSWORD_WP}
 
-wp core install --allow-root --url=${URL_WP} --title=${TITLE}  --admin_user=${ADMIN_WP} --admin_password=${ADMIN_PASSWORD_WP} --admin_email=${ADMIN_EMAIL_WP}
-wp user create --allow-root ${USER_WP}  ${USER_EMAIL_WP} --role=editor --user_pass=${USER_PASSWORD_WP}
+exec "$@"
 
-rm -rf /tmp/script.sh
-
-
-
-php-fpm8.2 -F
